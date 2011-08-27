936c9ef5b6ba32a4bd7b50ab1a3fa39978bcb223
Phillip <djwysrd@gmail.com>
2011-08-27 11:56:27 -0700

    resolved merge from haml branch, re-added type field to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-<<<<<<< HEAD
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :type
-=======
+
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
->>>>>>> haml
 end
