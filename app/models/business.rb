4bae012a7e20397c01b4f39874b636bc10ebc62f
Blake Miller <blak3mill3r@gmail.com>
2011-09-25 17:35:46 -0700

    business has_many deals, deals many-to-many with schools for eligibility whitelist

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,5 @@
 class Business < ActiveRecord::Base
   has_many :users
   has_many :locations, :foreign_key => 'organization_id'
-  
+  has_many :deals
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110906050919) do
+ActiveRecord::Schema.define(:version => 20110925234914) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -36,6 +36,21 @@ ActiveRecord::Schema.define(:version => 20110906050919) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "deal_school_eligibility", :id => false, :force => true do |t|
+    t.integer "school_id"
+    t.integer "deal_id"
+  end
+
+  add_index "deal_school_eligibility", ["deal_id", "school_id"], :name => "index_deal_school_eligibility_on_deal_id_and_school_id"
+
+  create_table "deals", :force => true do |t|
+    t.string  "name"
+    t.text    "description"
+    t.integer "business_id"
+  end
+
+  add_index "deals", ["business_id"], :name => "index_deals_on_business_id"
 
   create_table "locations", :force => true do |t|
     t.string   "name"
