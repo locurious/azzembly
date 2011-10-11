5dbcceee02946947c67bdf0a2a2d50ff3f6801ba
Blake Miller <blak3mill3r@gmail.com>
2011-10-11 03:14:38 -0700

    oops left debugger point in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,6 @@ class HomeController < ApplicationController
   
   def school
     @user_type = USER_TYPE_SCHOOL
-require 'ruby-debug';debugger; puts ""
     @school = current_user.school
   end
   
